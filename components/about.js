582cb3859fcd013c558caf8418586a703e19c979
Tay <taylor.mleigh@gmail.com>
2020-05-03 17:07:13 -0500

    skills table added

--- components/about.js
+++ components/about.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Image from "../components/image";
 import Layout from "../components/layout";
+import Skills from "../components/skillsTable";
 
 const container = {
   display: `flex`, 
   flexFlow: `row no-wrap`, 
-  // justifyContent: `flex-start`, 
-  // overflow: `auto`
 };
 
 const summary = {
@@ -38,12 +37,8 @@ const About = ({source, altern = "image"}) => {
           <h3><b>Software Engineer</b> | <i>Austin, TX</i></h3>
           <h6><mark>I'm currently open to job and project opportunities</mark>
             <p/>{`I’m a front-end engineer with a full-stack background. I have experience building with anything javascript and I’ve completed projects mostly with the SERN stack. I also enjoy spending my time actively exploring new libraries, frameworks, and tools. What I love most about this industry is that there’s always something new to learn and you’re only ever touching the tip of the iceberg with any given technology.`}
-            <p/>{`Languages:  JavaScript /CSS/ HTML`}
-            <br/>{`Frameworks:  React / JQuery / Express / Redux`}
-            <br/>{`Databases: 	mySQL`}
-            <br/>{`Testing: 	Mocha / Chai / Jasmine`}
-            <br/>{`Deployment: 	AWS / Elastic Beanstalk / Docker`}
-            <br/>{`Other:	 	Node.js / Git / Webpack / Babel / Agile / TDD / RESTful API / NextJS / GatsbyJS`}
+            <p/>
+            <Skills />
           </h6>
         </div>
       </div>
---
+++ components/skillsTable.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+const Skills = () => {
+  return (
+    <table><tbody>
+      <tr>
+        <td><i>Languages</i></td>
+        <td>JavaScript /CSS/ HTML</td>  
+      </tr>
+      <tr>
+        <td><i>Frameworks</i></td>
+        <td>React / JQuery / Express / Redux</td>  
+      </tr>
+      <tr>
+        <td><i>Databases</i></td>
+        <td>mySQL</td>  
+      </tr>
+      <tr>
+        <td><i>Testing</i></td>
+        <td>Mocha / Chai / Jasmine</td>  
+      </tr>
+      <tr>
+        <td><i>Deployment</i></td>
+        <td>AWS / Elastic Beanstalk / Docker</td>  
+      </tr>
+      <tr>
+        <td><i>Other</i></td>
+        <td>Node.js / RESTful API / Next.js / Gatsby.js / TDD </td>  
+      </tr>
+    </tbody></table>
+  )
+}
+
+export default Skills;
