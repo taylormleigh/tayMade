15b859566cdb34a1a7f9522d795ca0fb0eb85f59
Tay <taylor.mleigh@gmail.com>
2020-05-03 13:50:27 -0500

    styling

--- components/about.js
+++ components/about.js
@@ -9,11 +9,12 @@ var summary = {
 };
 
 var photo = {
-  float: `left`,
+  // float: `left`
 };
 
 var container = {
   display: `flex`, 
+  flexWrap: `nowrap`,
   flexDirection: `row`, 
   justifyContent: `flex-start`, 
   overflow: `auto`
@@ -27,10 +28,12 @@ const About = ({source, altern = "image"}) => {
           <br/><Image source="profile.jpeg" altern="profile"/>
         </div>
         <div style={summary}>
+          <h1>Tay Huval</h1>
           <h3><b>Software Engineer</b> | <i>Austin, TX</i></h3>
           <h6><mark>I'm currently open to job and project opportunities</mark>
-          <p/>My name is Tay and I’m a front-end engineer with a full-stack background. I have experience building with anything javascript and I’ve completed projects mostly with the SERN stack. I also enjoy spending my time actively exploring new libraries, frameworks, and tools. What I love most about this industry is that there’s always something new to learn and you’re only ever touching the tip of the iceberg with any given technology.
-          <p/>{`I’ve worked to leverage my past skills in art and design to effectively sharpen my skills as an engineer. I find that programming satisfies both my creative instincts and an inexplicable desire to sit and stare at a screen for long periods of time doing tedious amounts of work : )`}</h6>
+            <p/>{`I’m a front-end engineer with a full-stack background. I have experience building with anything javascript and I’ve completed projects mostly with the SERN stack. I also enjoy spending my time actively exploring new libraries, frameworks, and tools. What I love most about this industry is that there’s always something new to learn and you’re only ever touching the tip of the iceberg with any given technology.`}
+            <p/>{``}
+          </h6>
         </div>
       </div>
     </Layout>
--- components/header.js
+++ components/header.js
@@ -2,35 +2,38 @@ import Link from 'next/link';
 import React from "react"
 
 
+const flex = {
+  marginTop: `50px`,
+  display: `flex`,
+  flexDirection: `row`,
+  justifyContent: `center`,
+  width: `100%`,
+  padding: `4px`
+}
+
 const linkStyle = {
   marginRight: 15,
   color: `black`,
-  fontFamily: "Cabin",
+  fontFamily: "Yeseva One",
   textDecoration: `none`,
   textTransform: `uppercase`,
 };
 
-const alignStyle = {
-  margin: `0 auto`,
-  maxWidth: 920,
-  padding: `20px`,
-}
-
-const titleStyle = {
-  color: `black`,
-  marginBottom: `2px`,
+const buttons = {
+  display: `flex`,
+  height: `50px`,
+  alignItems: `center`,
+  justifyContent: `center`,
 }
 
 const Header = () => (
-    <div style={alignStyle}>
-      <center>
-      <h1 style={titleStyle}>Tay Huval
-      </h1>
-      <Link href="/"><a style={linkStyle}>About</a></Link>  
-      <Link href="/apps"><a style={linkStyle}>Apps</a></Link>  
-      <a href={'https://github.com/taylormleigh'} style={linkStyle}>GitHub</a>
-      <a href={'https://www.linkedin.com/in/tay-huval/'} style={linkStyle}>LinkedIn</a>
-      </center>
+    <div style={flex}>
+      <div style={buttons}>
+        <Link href="/"><a style={linkStyle}>About</a></Link>  
+        <Link href="/apps"><a style={linkStyle}>Apps</a></Link>  
+        <a href={'https://github.com/taylormleigh'} style={linkStyle}>GitHub</a>
+        <a href={'https://www.linkedin.com/in/tay-huval/'} style={linkStyle}>LinkedIn</a>
+      </div>
     </div>
 )
 
