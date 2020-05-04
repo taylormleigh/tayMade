151532736ab592fcd5a838150c09dc50998cb132
Tay <taylor.mleigh@gmail.com>
2020-05-04 00:33:33 -0500

    app demos thumbnails

--- pages/apps.js
+++ pages/apps.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-// import Image from "../components/image";
+import Image from "../components/image";
 
 const container = {
   display: `flex`, 
@@ -11,15 +11,24 @@ const container = {
 const Projects = () => (
   <Layout>
     <h1 style={container}>Live App Demos Coming Soon</h1>
-    {/* <ul>
+    <ul>
       <li>
-        <Image source="profile.jpeg" altern="profile"/>
+        {/* <a href="http://papergarden.tay-made.com"> */}
+          <Image source="papergarden.jpeg" altern="Paper & Garden"/>
+        {/* </a> */}
       </li>
       <li>
-        <Image source="profile.jpeg" altern="profile"/>
+        {/* <a href="http://sunflower.tay-made.com"> */}
+          <Image source="sunflower.jpeg" altern="Sunflower Fool"/>
+        {/* </a> */}
+      </li>
+      <li>
+        <a href="https://saskatchewanazon.herokuapp.com/">
+          <Image source="saskatch.jpeg" altern="Saskatchewanazon"/>
+        </a>
       </li>
       <li></li>
-    </ul> */}
+    </ul>
   </Layout>
 )
 
