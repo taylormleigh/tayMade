f0cb5f0d0e6b1fb75d2f385cabacfb48648b20fc
Tay <taylor.mleigh@gmail.com>
2020-05-04 01:07:09 -0500

    apps + demos + styling

--- pages/apps.js
+++ pages/apps.js
@@ -10,25 +10,54 @@ const container = {
 
 const Projects = () => (
   <Layout>
-    <h1 style={container}>Live App Demos Coming Soon</h1>
-    <ul>
-      <li>
-        {/* <a href="http://papergarden.tay-made.com"> */}
-          <Image source="papergarden.jpeg" altern="Paper & Garden"/>
-        {/* </a> */}
-      </li>
-      <li>
-        {/* <a href="http://sunflower.tay-made.com"> */}
-          <Image source="sunflower.jpeg" altern="Sunflower Fool"/>
-        {/* </a> */}
-      </li>
-      <li>
-        <a href="https://saskatchewanazon.herokuapp.com/">
-          <Image source="saskatch.jpeg" altern="Saskatchewanazon"/>
-        </a>
-      </li>
-      <li></li>
-    </ul>
+    {/* <h1 style={container}>Live App Demos Coming Soon</h1> */}
+      <table><tbody>
+        <tr>
+          <td>
+            {/* <a href="http://papergarden.tay-made.com"> */}
+              <Image source="papergarden.jpeg" altern="Paper & Garden"/>
+            {/* </a> */}
+          </td>
+          <td>
+            <h1>Paper & Garden</h1>
+            <h6>
+              A web app which assists storytellers with world-building
+              <br/><i>React / CSS / Node.js / Express / mySQL</i>
+              <br/> LIVE DEMO COMING SOON
+            </h6>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <a href="https://saskatchewanazon.herokuapp.com/">
+              <Image source="saskatch.jpeg" altern="Saskatchewanazon"/>
+            </a>
+          </td>
+          <td><h1>
+            <b>Saskatchewanazon</b></h1>
+            <h6>A Canadian-themed product image site mirroring Amazon’s style
+            <br/><i>CSS / React / Node.js / Express / mySQL / AWS / EC2 / RDS / Docker
+            <br/>(re-deployed to Heroku)</i>
+
+          </h6></td>
+        </tr>
+        <tr>
+          <td>
+            {/* <a href="http://sunflower.tay-made.com"> */}
+              <Image source="sunflower.jpeg" altern="Sunflower Fool"/>
+            {/* </a> */}
+          </td>
+          <td>
+          <h1>Sunflower Fool</h1>
+            <h6>
+              A form-based web app for recording tarot readings
+              <br/><i>React / CSS / Node.js / Next.js / Express / mySQL</i>
+              <br/> LIVE DEMO COMING SOON
+            </h6>
+          </td>
+        </tr>
+      </tbody></table>
+
   </Layout>
 )
 
