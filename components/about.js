2a2b0cc2a893f524b6b0423332905499e3334fa2
Tay <taylor.mleigh@gmail.com>
2020-07-02 15:15:37 -0500

    mobile responsive

--- components/about.js
+++ components/about.js
@@ -2,43 +2,39 @@ import React from "react";
 import Layout from "../components/layout";
 import Skills from "../components/skillsTable";
 
-const container = {
-  display: `flex`, 
-  flexFlow: `row no-wrap`, 
-};
-
-const summary = {
-  marginLeft: `20px`,
-  order: `2`,
-  overflow: `auto`,
-  float: `right`
-};
-
 const linkStyle = {
   marginRight: 15,
   color: `#971500`,
   fontFamily: "Roboto",
+  fontSize: `80%`,
   textDecoration: `none`,
 };
+
+const imgStyle = {
+  height: `100px`,
+  width: `100px`,
+  borderRadius: `50%`,
+}
 
 const About = () => {
   return (
     <Layout>
-      <div style={container}>
-        {/* <div style={photo}>
-            <Image source="profile.jpeg" altern="profile"/>
-        </div> */}
-        <div style={summary}>
+      <div className="section group">
+        <div className="col span_1_of_2">
+            <img src="profile.jpeg" style={imgStyle}/>
+        </div>
+        <div className="col span_1_of_2">
+
           <h1>Tay Huval</h1>
           <h3><strong>Software Engineer</strong> | <i>Austin, TX</i></h3>
           <mark><a style={linkStyle} className="textLink" href="mailto:me@taylor-made.com"><i>I'm currently open to job and project opportunities</i></a></mark>
+          </div>
+      </div>
           <h6>
-            <p/>{`Hi, I'm Tay! I’m a front-end engineer with a full-stack background. I have experience building with anything JavaScript and I’ve completed projects mostly using the SERN stack. I enjoy spending my time actively exploring new libraries, frameworks, and tools. What I love most about this industry is that there’s always something new to learn and you’re only ever touching the tip of the iceberg with any given technology. My first career was as an animator/illustrator and I’ve worked to leverage my past skills in art and design to effectively sharpen my skills as an engineer. My past also gives me an edge in sitting at a computer screen for long periods of time doing tedious amounts of work ;)`}
+            <p/>{`I’m a front-end engineer with a full-stack background. I have experience building with anything javascript and I’ve completed projects mostly with the SERN stack. My first career was as an animator/illustrator and I’ve worked to leverage my past skills in art and design to effectively sharpen my skills as an engineer. I find that programming satisfies both my creative instincts and an inexplicable desire to sit and stare at a screen for long periods of time doing tedious amounts of work :)`}
             <p/>
             <Skills />
           </h6>
-        </div>
-      </div>
     </Layout>
   );
 }
--- pages/apps.js
+++ pages/apps.js
@@ -15,59 +15,60 @@ const tableStyle = {
 
 const Projects = () => (
   <Layout>
-    {/* <h1 style={container}>Live App Demos Coming Soon</h1> */}
-    <div style={container}>
-      <table><tbody>
-        <tr>
-          <td>
-            <a href="http://papergarden.tay-made.com">
+<>
+  
+  <div className="section group">
+    <div className="col span_1_of_2">
+        <a href="http://papergarden.tay-made.com">
               <Image source="papergarden.jpeg" altern="Paper & Garden"/>
             </a>
-          </td>
-          <td>
-            <a href="http://papergarden.tay-made.com">
-              <h1>Paper & Garden</h1>
-            </a>
-            <h6>
-              A web app which assists storytellers with world-building
-              <br/><i>React / CSS / Node.js / Express / mySQL</i>
-              {/* <br/> LIVE DEMO COMING SOON */}
-            </h6>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <a href="https://saskatchewanazon.herokuapp.com/">
-              <Image source="saskatch.jpeg" altern="Saskatchewanazon"/>
-            </a>
-          </td>
-          <td>
-            <a href="https://saskatchewanazon.herokuapp.com/">
-              <h1>Saskatchewanazon</h1>
-            </a>
-            <h6>A Canadian-themed product image site mimicking Amazon’s style
-            <br/><i>CSS / React / Node.js / Express / mySQL / AWS / EC2 / RDS / Docker
-            <br/>(& re-deployed to Heroku)</i>
+    </div>
+    <div className="col span_1_of_2">
+      <a href="http://papergarden.tay-made.com">
+        <h1>Paper & Garden</h1>
+      </a>
+      <h6>
+        A web app which assists storytellers with world-building
+        <br/><i>React / CSS / Node.js / Express / mySQL</i>
+      </h6>
+    </div>
+  </div>
 
-          </h6></td>
-        </tr>
-        <tr>
-          <td>
-            {/* <a href="http://sunflower.tay-made.com"> */}
-              <Image source="sunflower.jpeg" altern="Sunflower Fool"/>
-            {/* </a> */}
-          </td>
-          <td>
+    <div className="section group">
+      <div className="col span_1_of_2">
+        <a href="https://saskatchewanazon.herokuapp.com/">
+          <Image source="saskatch.jpeg" altern="Saskatchewanazon"/>
+        </a>
+      </div>
+      <div className="col span_1_of_2">
+          <a href="https://saskatchewanazon.herokuapp.com/">
+            <h1>Saskatchewanazon</h1>
+          </a>
+          <h6>A Canadian-themed product image site mimicking Amazon’s style
+          <br/><i>CSS / React / Node.js / Express / mySQL / AWS / EC2 / RDS / Docker
+          <br/>(& re-deployed to Heroku)</i>
+
+        </h6>
+      </div>
+    </div>
+    <div className="section group">
+      <div className="col span_1_of_2">   
+        {/* <a href="http://sunflower.tay-made.com"> */}
+          <Image source="sunflower.jpeg" altern="Sunflower Fool"/>
+        {/* </a> */}
+      </div>
+      <div className="col span_1_of_2"> 
           <h1>Sunflower Fool</h1>
             <h6>
               A journal-like web app for recording tarot reading results and personal notes
               <br/><i>React / CSS / Node.js / Next.js / Express / mySQL</i>
               <br/> LIVE DEMO COMING SOON
             </h6>
-          </td>
-        </tr>
-      </tbody></table>
+      </div>
     </div>
+
+    
+</>
   </Layout>
 )
 
