63389f8a8fd36836eb46d0e82439a614e294cbe2
Tay <taylor.mleigh@gmail.com>
2020-12-22 20:22:53 -0600

    cleaning code for styling about me page

--- components/about.js
+++ components/about.js
@@ -1,35 +1,19 @@
 import Layout from "../components/layout";
 import Skills from "../components/skillsTable";
 
-const linkStyle = {
-  marginRight: 15,
-  color: `#971500`,
-  fontFamily: "Roboto",
-  fontSize: `0.7rem`,
-  textDecoration: `none`,
-};
 
-const imgStyle = {
-  minHeight: `30vh`,
-  maxHeight: `40vh`,
-  minWidth: `10vw`,
-  maxWidth: `20vw`,
-  borderRadius: `100%`,
-  marginLeft: 0,
-  marginBottom: 0,
-}
 
 const About = () => {
   return (
     <Layout>
       <div className="section group">
         <div className="col span_1_of_2">
-            <img src="photo.jpeg" style={imgStyle}/>
+            <img src="photo.jpeg" className="aboutPic"/>
         </div>
         <div className="col span_1_of_2">
           <h1>Tay Huval</h1>
           <h3><strong>Software Engineer</strong> | <i>Austin, TX</i></h3>
-          <mark><a style={linkStyle} className="textLink" href="mailto:me@taylor-made.com"><i>Currently open to job and project opportunities</i></a></mark>
+          <mark><a className="contactLink, textLink" href="mailto:me@taylor-made.com"><i>CONTACT</i></a></mark>
           <br/><br/>
           <h6>
             <Skills />
