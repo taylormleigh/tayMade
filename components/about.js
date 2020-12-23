4f2d90619be9422869dd29c8dcfe0ea75df2b59a
Tay <taylor.mleigh@gmail.com>
2020-12-23 17:27:28 -0600

    detail styling: about me description

--- components/about.js
+++ components/about.js
@@ -24,7 +24,7 @@ const About = () => {
           animate={{opacity:1}} 
           transition={{delay: 1.6, duration: 1}}
           >
-            <mark><a className="contactLink textLink" href="mailto:me@taylor-made.com">▻<i> currently open to jobs and projects</i></a></mark>
+            <mark><a className="contactLink textLink" href="mailto:me@taylor-made.com">▻<i>currently open to jobs and projects</i></a></mark>
           </motion.div>
           <br/><br/>
           <h6>
@@ -33,7 +33,7 @@ const About = () => {
         </div>
       </div>
       <div className="aboutText">
-            {`Hello! I'm Tay and I am a software engineer proficient in building full-stack apps, with a strong focus toward front-end engineering. I have completed projects primarily using the SERN stack. My background is in animation and illustration, which nurtured instincts in art and design that I leverage to effectively sharpen my skills as a designer and engineer. Many of my personal hobbies involve an element of creative problem solving and working through puzzles, which initially led me to programming and has kept me passionate about what I do.`}
+            {`Hello! I'm Tay and I am a software engineer proficient in building full-stack apps, with a strong focus toward front-end engineering. I have completed projects primarily using the SERN stack. My background is in animation and illustration, which nurtured instincts in art and design that I leverage to effectively sharpen my skills as an engineer. Many of my personal hobbies involve an element of creative problem solving and working through puzzles, which initially led me to programming and has kept me passionate about what I do.`}
             <p/>
             {`Whether working on small personal projects from scratch, teaching myself new tools and frameworks, or jumping onto bigger projects to help support a team, I like to approach my work with energy, creativity, and an eye for detail. I am currently seeking engineering roles at a solid company that I can grow in, though I am open to contract work depending on the project.`}
             <p/>
