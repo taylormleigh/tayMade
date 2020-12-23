bcd8a2be037b2d95fdd2540adf9f613adf98d683
Tay <taylor.mleigh@gmail.com>
2020-12-23 17:13:20 -0600

    detail styling: grayscale transition about me photo

--- components/about.js
+++ components/about.js
@@ -7,7 +7,13 @@ const About = () => {
     <Layout>
       <div className="section group">
         <div className="col span_1_of_2">
+        <motion.div 
+          initial={{filter: `grayscale(100%)`}}
+          animate={{filter: `grayscale(0%)`}} 
+          transition={{delay: 1, duration: 1.4}}
+          >
             <img src="photo.jpeg" className="aboutPic"/>
+          </motion.div>
         </div>
 
         <div className="col span_1_of_2 aboutPage">
@@ -27,9 +33,9 @@ const About = () => {
         </div>
       </div>
       <div className="aboutText">
-            {`Hello! I'm Tay and I am a front-end engineer, though I'm proficient in building full-stack apps. I have completed projects primarily using the SERN stack. My background is in animation and illustration, which nurtured instincts in art and design that I leverage to effectively sharpen my skills as a designer and engineer. Many of my personal hobbies involve an element of creative problem solving and working through puzzles, which led me to programming initially and has kept me passionate about what I do.`}
+            {`Hello! I'm Tay and I am a software engineer proficient in building full-stack apps, with a strong focus toward front-end engineering. I have completed projects primarily using the SERN stack. My background is in animation and illustration, which nurtured instincts in art and design that I leverage to effectively sharpen my skills as a designer and engineer. Many of my personal hobbies involve an element of creative problem solving and working through puzzles, which initially led me to programming and has kept me passionate about what I do.`}
             <p/>
-            {`Whether working on small personal projects from scratch, teaching myself new tools and frameworks, or jumping onto bigger projects to help support a team, I like to approach my work with energy, excitement, and an eye for detail. I am currently seeking either Engineer I or II roles at a solid company that I can grow in, though I am open to contract work depending on the project.`}
+            {`Whether working on small personal projects from scratch, teaching myself new tools and frameworks, or jumping onto bigger projects to help support a team, I like to approach my work with energy, creativity, and an eye for detail. I am currently seeking engineering roles at a solid company that I can grow in, though I am open to contract work depending on the project.`}
             <p/>
             {`Feel free to contact me for inquiries and opportunities!`}
       </div>
