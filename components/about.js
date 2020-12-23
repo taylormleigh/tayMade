42e5e701865de597369111cf457f32989adc58b1
Tay <taylor.mleigh@gmail.com>
2020-12-23 12:48:02 -0600

    updated about me

--- components/about.js
+++ components/about.js
@@ -21,7 +21,11 @@ const About = () => {
           </div>
       </div>
       <div className="aboutText">
-            {`Hello! I'm Tay, a front-end engineer with experience building full-stack apps. I have completed projects primarily using the SERN stack. My background is in animation and illustration, which nurtured instincts in art and design that I leverage to effectively sharpen my skills as a designer and engineer. Whether working on small personal projects from scratch or jumping onto bigger projects to help support a team, I like to approach programming with energy, excitement, and an eye for detail. Feel free to contact me for inquiries and opportunities!`}
+            {`Hello! I'm Tay and I am a front-end engineer, though I'm proficient in building full-stack apps. I have completed projects primarily using the SERN stack. My background is in animation and illustration, which nurtured instincts in art and design that I leverage to effectively sharpen my skills as a designer and engineer. Many of my personal hobbies involve an element of creative problem solving and working through puzzles, which led me to programming initially and has kept me passionate about what I do.`}
+            <p/>
+            {`Whether working on small personal projects from scratch, teaching myself new tools and frameworks, or jumping onto bigger projects to help support a team, I like to approach my work with energy, excitement, and an eye for detail. I am currently seeking either Engineer I or II roles at a solid company that I can grow in, though I am open to contract work depending on the project.`}
+            <p/>
+            {`Feel free to contact me for inquiries and opportunities!`}
       </div>
     </Layout>
   );
