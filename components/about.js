0601644543a0cbd5d183a015eec06de25978c25d
Tay <taylor.mleigh@gmail.com>
2020-12-23 16:48:13 -0600

    detail styling: open to jobs delayed transition

--- components/about.js
+++ components/about.js
@@ -2,11 +2,6 @@ import Layout from "../components/layout";
 import Skills from "../components/skillsTable";
 import { motion } from "framer-motion";
 
-const variants = {
-  hidden: { opacity: 0 },
-  visible: { opacity: 1 },
-}
-
 const About = () => {
   return (
     <Layout>
@@ -14,15 +9,22 @@ const About = () => {
         <div className="col span_1_of_2">
             <img src="photo.jpeg" className="aboutPic"/>
         </div>
+
         <div className="col span_1_of_2 aboutPage">
           <h1>Tay Huval</h1>
           <h3><strong>Software Engineer</strong> | <i>Austin, TX</i></h3>
-          <mark><a className="contactLink textLink" href="mailto:me@taylor-made.com">▻<i> currently open to jobs and projects</i></a></mark>
+          <motion.div 
+          initial={{opacity:0}} 
+          animate={{opacity:1}} 
+          transition={{delay: 0.4, duration: 1.6}}
+          >
+            <mark><a className="contactLink textLink" href="mailto:me@taylor-made.com">▻<i> currently open to jobs and projects</i></a></mark>
+          </motion.div>
           <br/><br/>
           <h6>
             <Skills />
           </h6>
-          </div>
+        </div>
       </div>
       <div className="aboutText">
             {`Hello! I'm Tay and I am a front-end engineer, though I'm proficient in building full-stack apps. I have completed projects primarily using the SERN stack. My background is in animation and illustration, which nurtured instincts in art and design that I leverage to effectively sharpen my skills as a designer and engineer. Many of my personal hobbies involve an element of creative problem solving and working through puzzles, which led me to programming initially and has kept me passionate about what I do.`}
