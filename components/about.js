ba2a2d01dbbe905ad43b3aa53f6f8dcfef32b61a
Tay <taylor.mleigh@gmail.com>
2020-12-22 19:30:49 -0600

    about me

--- components/about.js
+++ components/about.js
@@ -29,14 +29,14 @@ const About = () => {
         <div className="col span_1_of_2">
           <h1>Tay Huval</h1>
           <h3><strong>Software Engineer</strong> | <i>Austin, TX</i></h3>
-          <mark><a style={linkStyle} className="textLink" href="mailto:me@taylor-made.com"><i>I'm currently open to job and project opportunities</i></a></mark>
+          <mark><a style={linkStyle} className="textLink" href="mailto:me@taylor-made.com"><i>Currently open to job and project opportunities</i></a></mark>
           <br/><br/>
           <h6>
             <Skills />
           </h6>
           </div>
       </div>
-            {`Tay is a front-end engineer with a full-stack background. She is experienced building apps and has completed projects primarily using the SERN stack. Whether working on small personal projects from scratch or jumping onto a bigger project to help support her team, programming is more than just a job. Her first career as an animator/illustrator nurtured instincts in art and design which she leverages to effectively sharpen her skills as an engineer.`}
+            {`Tay is a front-end engineer with a full-stack background. She is experienced building apps and has completed projects primarily using the SERN stack. Her first career as an animator/illustrator nurtured instincts in art and design which she leverages to effectively sharpen her skills as an engineer. Whether working on small personal projects from scratch or jumping onto a bigger project to help support a team, she approaches programming with energy, excitement, and an eye for detail.`}
     </Layout>
   );
 }
