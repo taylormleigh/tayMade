e378a19c22290ce72ae6ea476be3ccdd7bd2454f
Tay <taylor.mleigh@gmail.com>
2020-12-23 19:35:22 -0600

    code cleanup: layout variables to css classes

--- components/layout.js
+++ components/layout.js
@@ -1,27 +1,5 @@
 import Header from "./header";
 import { motion, AnimatePresence } from 'framer-motion';
-import Router from "next/dist/next-server/server/router";
-
-const bgStyle = {
-  backgroundColor: `#ffffff`,
-  margin: `0 auto`,
-  maxWidth: 800,
-  minHeight: `70vh`,
-  maxHeight: `90vh`,
-  overflow: `auto`,
-  padding: `10px`,
-  // borderTop: `1px`,
-  // borderBottom: `0px`,
-  // borderLeft: `0px`,
-  // borderRight: `0px`,
-  // borderStyle: `solid`,
-  // borderColor: `#d7bd3c`,
-};
-
-const divStyle = {
-  padding: `8px`,
-}
-
 
 const Layout = ({children}) => {
   return (
@@ -35,19 +13,15 @@ const Layout = ({children}) => {
       exit={{opacity:0}}
       transition={{duration: 1.1}}
       >
-        
-        <div style={bgStyle}>
-          <div style={divStyle}>
+        <div className="layoutBG">
+          <div className="layoutDiv">
               <main>{children}</main>
           </div>
         </div>
       </motion.div>
     </AnimatePresence>
-
-        <a href="mailto:me@taylor-made.com" className="contactButton">CONTACT</a>
-      
-        <a href="mailto:me@taylor-made.com" className="contactButtonMobile">contact</a>
-    
+      <a href="mailto:me@taylor-made.com" className="contactButton">CONTACT</a>
+      <a href="mailto:me@taylor-made.com" className="contactButtonMobile">contact</a>
     </>
   )
 }
