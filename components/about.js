f838ef6694dbb41bafe3fe6a392d0f70b218f2ea
Tay <taylor.mleigh@gmail.com>
2021-11-06 13:47:35 -0500

    removed looking for work from about me

--- components/about.js
+++ components/about.js
@@ -38,7 +38,9 @@ const About = () => {
       <div className="aboutText">
             {`Hello! I'm Tay and I am a software engineer proficient in building full-stack apps, with a strong focus toward front-end engineering. I have completed projects primarily using the SERN stack. My background is in animation and illustration, which nurtured instincts in art and design that I leverage to effectively sharpen my skills as an engineer. Many of my personal hobbies involve an element of creative problem solving and working through puzzles, which initially led me to programming and has kept me passionate about what I do.`}
             <p/>
-            {`Whether working on small personal projects from scratch, teaching myself new tools and frameworks, or jumping onto bigger projects to help support a team, I like to approach my work with energy, creativity, and an eye for detail. I am currently seeking engineering roles at a solid company that I can grow in, though I am open to contract work depending on the project.`}
+            {`Whether working on small personal projects from scratch, teaching myself new tools and frameworks, or jumping onto bigger projects to help support a team, I like to approach my work with energy, creativity, and an eye for detail.`} 
+            
+            {/* {`I am currently seeking engineering roles at a solid company that I can grow in, though I am open to contract work depending on the project.`} */}
             <p/>
             {`Feel free to contact me for inquiries and opportunities!`}
       </div>
